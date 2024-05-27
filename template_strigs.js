10ee9619711d217074eaa5be9b036da49de1cb3b
Mateo-Valenzuela <matep.d35@gmail.com>
2024-05-27 19:38:29 -0300

    html5

---
+++ template_strigs.js
@@ -0,0 +1,11 @@
+const n= 1
+const fecha= '27/05/2024'
+
+console.log('Mensaje de prueba: '+n+' fecha de hoy: '+fecha)
+
+//template string
+
+console.log(´
+   Mensaje de prueba: ${1}
+   Fecha de hoy: ${fecha}   
+´)
